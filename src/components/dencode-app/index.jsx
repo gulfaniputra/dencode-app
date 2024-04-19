6492743f5ef4040c226d80d9d9090aa62781e402
Gulfani Putra <gulfaniputra@gmail.com>
2024-04-19 20:57:09 +0800

    Create basic UI with vanilla HTML

---
+++ src/components/dencode-app/index.jsx
@@ -0,0 +1,12 @@
+export default function QRCodeGenerator() {
+  return (
+    <div>
+      <h1>Dencode</h1>
+      <h2>QR Code Generator</h2>
+      <div>
+        <input type="text" name="qr-code" placeholder="Enter your value here" />
+        <button>Generate</button>
+      </div>
+    </div>
+  );
+}
