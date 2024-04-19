2df74761d5e9ba36d5dede4984cc6eba465d5197
Gulfani Putra <gulfaniputra@gmail.com>
2024-04-19 21:30:45 +0800

    Implement 'react-qr-code'

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import QRCode from "react-qr-code";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [qrCode, setQrCode] = useState("");
+  const [input, setInput] = useState("");
+
+  function handleGenerateQrCode() {
+    setQrCode(input);
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+        <h1>Dencode</h1>
 
-export default App
+        <div>
+          <input
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            name="qr-code"
+            placeholder="Enter your value here"
+          />
+          <button
+            disabled={input && input.trim !== "" ? false : true}
+            onClick={handleGenerateQrCode}>
+            Generate QR Code
+          </button>
+        </div>
+
+        <div>
+          <QRCode id="qr-code-value" value={qrCode} size={400} />
+        </div>
+      </div>
+    </>
+  );
+}
